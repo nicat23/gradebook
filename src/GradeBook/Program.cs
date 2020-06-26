feb9618295be4fc1e6d9f956c0adb7050a11d42c
Justin P <52590296+nicat23@users.noreply.github.com>
2020-06-25 21:52:34 -0500

    Initial

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new double[] { 12.7, 10.3, 6.11, 4.1 };
+            List<double> grades = new List<double>(numbers);
+            double result = 0;
+
+            foreach (var item in grades)
+            {
+                Console.WriteLine(item);
+                result += item;
+            }
+            Console.WriteLine(result);
+
+            if (args.Length > 0)
+            {
+                //Console.WriteLine("Hello, Justin!");
+                //Console.WriteLine("Hello, " + args[0] + "!");
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
